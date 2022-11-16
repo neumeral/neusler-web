93c750546d63d308b489f3641a5a7e60bba68fd3
Awin Abi <awin@awinawin.awin>
2022-11-16 11:38:09 +0530

    navbar changes and more optimizations

--- neuslerweb/settings.py
+++ neuslerweb/settings.py
@@ -1,4 +1,5 @@
 import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,3 +117,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+APPEND_SLASH = False
